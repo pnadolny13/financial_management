72c316fe63647ea82125dbcd77e239a8d0e25b81
Pat Nadolny <patnadolny@gmail.com>
2020-04-12 11:30:32 -0400

    collect static files on deployment

--- deploy.sh
+++ deploy.sh
@@ -13,4 +13,4 @@ rm nginx.tar
 scp docker-compose.prod.yml ec2-user@$1:/home/ec2-user/docker-compose.prod.yml
 scp .env.prod ec2-user@$1:/home/ec2-user/.env.prod
 
-ssh ec2-user@$1 "mkdir /home/ec2-user/financial_management; mkdir /home/ec2-user/nginx; docker-compose -f /home/ec2-user/docker-compose.prod.yml up -d;"
+ssh ec2-user@$1 "docker-compose -f /home/ec2-user/docker-compose.prod.yml up -d; docker-compose -f /home/ec2-user/docker-compose.prod.yml exec -T web python manage.py collectstatic --no-input --clear;"
